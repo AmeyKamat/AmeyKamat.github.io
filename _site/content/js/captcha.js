14ea35660efb375b792e49acd1f390374454383c
Amey Kamat <ask5332@gmail.com>
2014-09-25 00:16:24 +0530

    Removing technival

---
+++ _site/content/js/captcha.js
@@ -0,0 +1,28 @@
+function DrawCaptcha()
+{
+    var a = Math.ceil(Math.random() * 10)+ '';
+    var b = Math.ceil(Math.random() * 10)+ '';       
+    var c = Math.ceil(Math.random() * 10)+ '';  
+	var d = Math.ceil(Math.random() * 10)+ '';  
+    var e = Math.ceil(Math.random() * 10)+ '';  
+    var f = Math.ceil(Math.random() * 10)+ '';  
+    var g = Math.ceil(Math.random() * 10)+ '';  
+    var code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' '+ f + ' ' + g;
+    document.getElementById("txtCaptcha").value = code
+}
+
+// Validate the Entered input aganist the generated security code function   
+function Validate(){
+    var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
+    var str2 = removeSpaces(document.getElementById('txtInput').value);
+    if (str1 == str2){
+		document.getElementById('navigation-buttons').innerHTML = '<button class="btn btn-primary" type="submit" name="submit" value="Submit" id="ss-submit">Submit</button>';
+	}
+        
+}
+
+// Remove the spaces from the entered and generated code
+function removeSpaces(string)
+{
+    return string.split(' ').join('');
+}
