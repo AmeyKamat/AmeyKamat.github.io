68679496480463c3061a60fb3d5bfdfb3c805f0d
Amey Kamat <ask5332@gmail.com>
2015-01-13 20:42:37 +0530

    New resume

--- content/js/canvasapp.js
+++ content/js/canvasapp.js
@@ -9,6 +9,83 @@ function Shape(t, x, y, w, h, fill) {
 	this.w = w || 1;
 	this.h = h || 1;
 	this.fill = fill || '#AAAAAA';
+	if(this.t == 0){
+		this.noOfHandles = 8;
+		this.resizeHandles = 	[
+									[this.x, this.y],
+									[(this.x + (this.w)/2), this.y],
+									[(this.x + this.w), this.y],
+									[(this.x + this.w), (this.y + (this.h)/2)],
+									[(this.x + this.w), (this.y + this.h)],
+									[(this.x + (this.w)/2), (this.y + this.h)],
+									[this.x, (this.y + this.h)],
+									[this.x, (this.y + (this.h)/2)]
+								];
+	}
+	else if(this.t == 1){
+		this.noOfHandles = 4;
+		this.resizeHandles = 	[
+									[(this.x - this.w), (this.y - this.w)],
+									[(this.x + this.w), (this.y - this.w)],
+									[(this.x + this.w), (this.y + this.w)],
+									[(this.x - this.w), (this.y + this.w)]
+								];
+	}
+}
+
+Shape.prototype.moveResizeHandles = function(){
+	if(this.t == 0){
+		this.resizeHandles = 	[
+									[this.x, this.y],
+									[(this.x + (this.w)/2), this.y],
+									[(this.x + this.w), this.y],
+									[(this.x + this.w), (this.y + (this.h)/2)],
+									[(this.x + this.w), (this.y + this.h)],
+									[(this.x + (this.w)/2), (this.y + this.h)],
+									[this.x, (this.y + this.h)],
+									[this.x, (this.y + (this.h)/2)]
+								];
+	}
+	else if(this.t == 1){
+		this.resizeHandles = 	[
+									[(this.x - this.w), (this.y - this.w)],
+									[(this.x + this.w), (this.y - this.w)],
+									[(this.x + this.w), (this.y + this.w)],
+									[(this.x - this.w), (this.y + this.w)]
+								];
+	}
+}
+
+Shape.prototype.resize = function(handle, offX, offY){
+	if(this.t == 0){
+		this.w += offX;
+	}
+	else if(this.t == 1){
+		switch(handle){
+			case 0: 	this.x += offX; this.y += offY;
+						this.w += offX; this.h += offY;
+						break;
+			case 1: 	this.y += offY;
+						this.h += offY;
+						break;
+			case 3: 	this.w += offX;
+						break;
+			case 2: 	this.y += offY;
+						this.w += offX; this.h += offY;
+						break;
+			case 5: 	this.h += offY;
+						break;
+			case 4: 	this.w += offX; this.h += offY;
+						break;
+			case 6: 	this.x += offX;
+						this.w += offX; this.h += offY;
+						break;
+			case 7: 	this.x += offX;
+						this.w += offX;
+						break;
+		}
+		this.moveResizeHandles();
+	}
 }
 
 // Draws this shape to a given context
@@ -40,6 +117,7 @@ function CanvasState(canvas) {
 	this.width = canvas.width;
 	this.height = canvas.height;
 	this.ctx = canvas.getContext('2d');
+	this.resizeDrag = false;
    
 	var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
 	if (document.defaultView && document.defaultView.getComputedStyle) {
@@ -52,6 +130,9 @@ function CanvasState(canvas) {
 	var html = document.body.parentNode;
 	this.htmlTop = html.offsetTop;
 	this.htmlLeft = html.offsetLeft;
+	this.currentResizePosX = 0;
+	this.currentResizePosY = 0;
+	
 
 	this.valid = false; // when set to false, the canvas will redraw everything
 	this.shapes = [];  // the collection of things to be drawn
@@ -69,33 +150,57 @@ function CanvasState(canvas) {
 		var mouse = myState.getMouse(ev.changedPointers[0]);
 		var mx = mouse.x;
 		var my = mouse.y;
-		
+		var activeHandle;
 		var shapes = myState.shapes;
-		var mySel
-		var l = shapes.length;
-		for (var i = l-1; i >= 0; i--) {
-			if (shapes[i].contains(mx, my)) {
-				mySel = shapes[i];
+		var mySel;
+		if((activeHandle = myState.inSelectedHandle(mx, my)) != -1){
+			myState.resizeDrag = true;
+			myState.currentResizePosX = myState.selection.resizeHandles[activeHandle][0];
+			myState.currentResizePosY = myState.selection.resizeHandles[activeHandle][1];
+		}
+		else{
+			var l = shapes.length;
+			for (var i = l-1; i >= 0; i--) {
+				if (shapes[i].contains(mx, my)) {
+					mySel = shapes[i];
+				}
+			}
+			myState.dragoffx = mx - mySel.x;
+			myState.dragoffy = my - mySel.y;
+			myState.selection = mySel;
+			myState.valid = false;
+			if(myState.selection){
+				myState.dragging = true;
 			}
 		}
-		myState.selection = mySel;
-		if(myState.selection){
-			myState.dragging = true;
-		}
-		
 	});
 	hammertime.on('panend pancancel', function(ev) {
 		myState.dragging = false;
+		myState.resizeDrag = false;
+		myState.valid = false;
 	});
 	hammertime.on('panmove', function(ev) {
-		if(myState.dragging){
+		if(myState.resizeDrag){
+			console.log("dragging");
+			activeHandle = myState.inSelectedHandle(mx, my);
 			var mouse = myState.getMouse(ev.changedPointers[0]);
 			var mx = mouse.x;
 			var my = mouse.y;
-			
+			myState.selection.resize(activeHandle, mouse.x - this.currentRezizePosX, mouse.y - this.currentRezizePosY);
+			this.currentRezizePosX = mouse.x;
+			this.currentRezizePosY = mouse.y;
+			myState.valid = false;
+		}
+		else if(myState.dragging){
+			var mouse = myState.getMouse(ev.changedPointers[0]);
+			var mx = mouse.x;
+			var my = mouse.y;
+			var originalX = myState.selection.x, originalY = myState.selection.y;
 			
 				myState.selection.x = mouse.x - myState.dragoffx;
-				myState.selection.y = mouse.y - myState.dragoffy;   
+				myState.selection.y = mouse.y - myState.dragoffy;
+				myState.selection.moveResizeHandles(myState.selection.x - originalX, myState.selection.y - originalY);
+				
 				myState.valid = false; // Something's dragging so we must redraw
 		}
 	});
@@ -106,6 +211,7 @@ function CanvasState(canvas) {
 			
 			var mx = mouse.x;
 			var my = mouse.y;
+			
 			var shapes = myState.shapes;
 			var mySel
 			var l = shapes.length;
@@ -128,6 +234,7 @@ function CanvasState(canvas) {
 		
 			var mx = mouse.x;
 			var my = mouse.y;
+			console.log(myState.inSelectedHandle(mx, my));
 			var shapes = myState.shapes;
 			var l = shapes.length;
 			for (var i = l-1; i >= 0; i--) {
@@ -149,13 +256,11 @@ function CanvasState(canvas) {
 			myState.valid = false; // Need to clear the old selection border
 		}
 	});
-	canvas.addEventListener('mousedown', function(e) {
-		
-	}, true);
 	
 	  
 	this.selectionColor = '#CC0000';
-	this.selectionWidth = 2;  
+	this.selectionWidth = 2; 
+	this.resizeHandleRadius = 8;
 	this.interval = 30;
 	setInterval(function() { myState.draw(); }, myState.interval);
 }
@@ -163,6 +268,21 @@ function CanvasState(canvas) {
 CanvasState.prototype.addShape = function(shape) {
   this.shapes.push(shape);
   this.valid = false;
+}
+
+CanvasState.prototype.inSelectedHandle = function(mx, my){
+	var newShape;
+	if(this.selection != null){
+		var shapes = this.shapesl
+		var mySel = this.selection;
+		for(var i = 0; i<mySel.noOfHandles; i++){
+				newShape = new Shape(1, mySel.resizeHandles[i][0], mySel.resizeHandles[i][1], 8, -1, "black");
+				if(newShape.contains(mx,my)){
+					return i;
+				}
+		}
+		return -1;
+	}
 }
 
 CanvasState.prototype.clear = function() {
@@ -174,6 +294,7 @@ CanvasState.prototype.draw = function() {
   if (!this.valid) {
     var ctx = this.ctx;
     var shapes = this.shapes;
+    console.log(shapes[shapes.length-1].w);
     this.clear();
         
     var l = shapes.length;
@@ -184,7 +305,7 @@ CanvasState.prototype.draw = function() {
           shape.x + shape.w < 0 || shape.y + shape.h < 0) continue;
       shapes[i].draw(ctx);
     }
-    
+    this.drawSelection();
     // draw selection
     // right now this is just a stroke along the edge of the selected Shape
     /*if (this.selection != null) {
@@ -200,7 +321,34 @@ CanvasState.prototype.draw = function() {
   }
 }
 
-
+CanvasState.prototype.drawSelection = function(){
+	var width, height, x, y;
+	if(this.selection != null){
+		width = this.selection.w;
+		if(this.selection.t == 1){
+			console.log("hi");
+			x = this.selection.x - width;
+			y = this.selection.y - width;
+			width += width;
+			height = width;
+		}
+		else{
+			height = this.selection.h;
+			x = this.selection.x;
+			y = this.selection.y;
+		}
+		var ctx = this.ctx;
+		ctx.strokeStyle = this.selectionColor;
+		ctx.lineWidth = this.selectionWidth;
+		ctx.strokeRect(x,y,width,height);
+		for(var i=0; i<this.selection.noOfHandles; i++){
+			ctx.fillStyle = this.selectionColor;
+			ctx.beginPath();
+			ctx.arc(this.selection.resizeHandles[i][0],this.selection.resizeHandles[i][1],this.resizeHandleRadius,0,2*Math.PI);
+			ctx.fill();
+		}
+	}
+}
 
 CanvasState.prototype.getMouse = function(e) {
   var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
