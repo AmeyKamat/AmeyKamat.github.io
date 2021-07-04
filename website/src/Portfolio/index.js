8c46798550491162d4374939d87439a72cfbee43
Amey <amey@Ameys-MacBook-Pro.local>
2021-07-04 17:24:43 +0530

    Removing project modal

--- website/src/Portfolio/index.js
+++ website/src/Portfolio/index.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Grid } from "@material-ui/core"
 
 import Heading from '../Heading'
 import Section from "../Section"
 import PortfolioItem from '../PortfolioItem'
-import PortfolioModal from '../PortfolioModal'
 
 import "./index.css"
 
@@ -13,11 +12,6 @@ import projects from "./projects"
 
 const Portfolio = () => {
 
-    const [ open, setOpen ] = useState(false)
-    const [ selected, setSelected ] = useState({})
-
-    const handleClick= project => { setSelected(project); setOpen(true) }
-
     return <Section>
         <Grid container justify="center">
             <Grid item xs={12}>
@@ -25,10 +19,9 @@ const Portfolio = () => {
             </Grid>
             <Grid container className="portfolio-box" spacing={3} justify="center">
                 {
-                    projects.map((project, i) => <PortfolioItem project={project} key={i} onClick={handleClick}/>)
+                    projects.map((project, i) => <PortfolioItem project={project} key={i} />)
                 }
             </Grid>
-            <PortfolioModal project={selected} isOpen={open} onClose={() => setOpen(false)} />
         </Grid>
     </Section>
 }
